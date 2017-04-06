dbce7ad8c92a4b403dee2696c3bdd8ee6c3654c3
Fadri Furrer <ffurrer@gmail.com>
2017-04-06 17:54:04 +0200

    added test for noisy poses

--- hand_eye_calibration/test/test_hand_eye_calibration.py
+++ hand_eye_calibration/test/test_hand_eye_calibration.py
@@ -57,8 +57,9 @@ class HandEyeCalibration(unittest.TestCase):
 
   def test_hand_eye_calibration_with_outliers(self):
     dq_B_H_vec, dq_W_E_vec = he_helpers.generate_test_paths(
-        20, self.dq_H_E, self.dq_B_W, self.paths_start_at_origin, True, 0.1,
-        True, 0.5)
+        20, self.dq_H_E, self.dq_B_W, self.paths_start_at_origin,
+        include_outliers_B_H=True, outlier_probability_B_H=0.1,
+        include_outliers_W_E=True, outlier_probability_W_E=0.5)
 
     # Plot the poses with outliers.
     poses_B_H = np.array([dq_B_H_vec[0].to_pose().T])
@@ -92,8 +93,48 @@ class HandEyeCalibration(unittest.TestCase):
         "input dual quaternion: {}, estimated dual quaternion: {}".format(
             self.dq_H_E, dq_H_E_estimated))
 
+  def test_hand_eye_calibration_with_noise(self):
+    dq_B_H_vec, dq_W_E_vec = he_helpers.generate_test_paths(
+        20, self.dq_H_E, self.dq_B_W, self.paths_start_at_origin,
+        include_outliers_B_H=False, include_noise_B_H=True,
+        noise_sigma_trans_B_H=0.0001, noise_sigma_rot_B_H=0.001,
+        include_outliers_W_E=False, include_noise_W_E=True,
+        noise_sigma_trans_W_E=0.0001, noise_sigma_rot_W_E=0.001)
+
+    # Plot the poses with noise.
+    poses_B_H = np.array([dq_B_H_vec[0].to_pose().T])
+    poses_W_E = np.array([dq_W_E_vec[0].to_pose().T])
+    for i in range(1, len(dq_B_H_vec)):
+      poses_B_H = np.append(poses_B_H, np.array(
+          [dq_B_H_vec[i].to_pose().T]), axis=0)
+      poses_W_E = np.append(poses_W_E, np.array(
+          [dq_W_E_vec[i].to_pose().T]), axis=0)
+    draw_poses(poses_B_H, poses_W_E)
+
+    dq_H_E_estimated = align(dq_B_H_vec, dq_W_E_vec,
+                             self.enforce_same_non_dual_scalar_sign)
+    pose_H_E_estimated = dq_H_E_estimated.to_pose()
+    dq_H_E_estimated.normalize()
+
+    assert pose_H_E_estimated[6] > 0.0, (
+        "The real part of the pose's quaternion should be positive. "
+        "The pose is: \n{}\n where the dual quaternion was: "
+        "\n{}".format(pose_H_E_estimated, dq_H_E_estimated))
+
+    print("The static input pose was: \n{}".format(self.pose_H_E))
+    print("The hand-eye calibration's output pose is: \n{}".format(
+          pose_H_E_estimated))
+
+    print("T_H_E ground truth: \n{}".format(self.dq_H_E.to_matrix()))
+    print("T_H_E estimated: \n{}".format(dq_H_E_estimated.to_matrix()))
+
+    assert np.allclose(
+        self.dq_H_E.dq, dq_H_E_estimated.dq, rtol=4e-2), (
+        "input dual quaternion: {}, estimated dual quaternion: {}".format(
+            self.dq_H_E, dq_H_E_estimated))
+
   def test_draw_poses(self):
-      # Draw both paths in their Global/World frame.
+    # Draw both paths in their Global/World frame.
     poses_B_H = np.array([self.dq_B_H_vec[0].to_pose().T])
     poses_W_E = np.array([self.dq_W_E_vec[0].to_pose().T])
     for i in range(1, len(self.dq_B_H_vec)):
