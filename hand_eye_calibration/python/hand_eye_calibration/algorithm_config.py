1853278165cb05163b94f0c93b5700342f4a541f
marius <marius.b.fehr@gmail.com>
2017-04-09 22:50:42 +0200

    make prefiltering more strict

--- hand_eye_calibration/python/hand_eye_calibration/algorithm_config.py
+++ hand_eye_calibration/python/hand_eye_calibration/algorithm_config.py
@@ -29,7 +29,7 @@ def get_basic_config():
 
   # Select distinctive poses based on skrew axis
   hand_eye_config.prefilter_poses_enabled = False
-  hand_eye_config.prefilter_dot_product_threshold = 0.975
+  hand_eye_config.prefilter_dot_product_threshold = 0.95
 
   # RANSAC
   hand_eye_config.ransac_sample_size = 3
