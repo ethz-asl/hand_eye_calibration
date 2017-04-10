6f4df54819954cfc4cc232d9de174c9e83ab9ea3
Fadri Furrer <ffurrer@gmail.com>
2017-04-11 00:29:17 +0200

    add some more text to the description and fix line widths

--- hand_eye_calibration/scripts/generate_plots.py
+++ hand_eye_calibration/scripts/generate_plots.py
@@ -13,9 +13,10 @@ font.set_size('small')
 font.set_family('serif')
 font.set_weight('light')
 font.set_style('normal')
+line_width = 2
 
 
-def generate_box_plot(methods, position_rmses, orientation_rmses):
+def generate_box_plot(dataset, methods, position_rmses, orientation_rmses):
 
   num_methods = len(methods)
   x_ticks = np.linspace(0, 1, num_methods)
@@ -25,16 +26,27 @@ def generate_box_plot(methods, position_rmses, orientation_rmses):
   fig, ax1 = plt.subplots()
   ax1.set_ylabel('RMSE position [m]', color='b')
   ax1.tick_params('y', colors='b')
-  fig.suptitle("Hand-Eye Calibration Method Comparison", fontsize='24')
-  ax1.boxplot(position_rmses, 0, '', positions=x_ticks - spacing, widths=width)
+  fig.suptitle(
+      "Hand-Eye Calibration Method Error {}".format(dataset), fontsize='24')
+  bp_position = ax1.boxplot(position_rmses, 0, '',
+                            positions=x_ticks - spacing, widths=width)
+  plt.setp(bp_position['boxes'], color='blue', linewidth=line_width)
+  plt.setp(bp_position['whiskers'], color='blue', linewidth=line_width)
+  plt.setp(bp_position['fliers'], color='blue',
+           marker='+', linewidth=line_width)
+  plt.setp(bp_position['caps'], color='blue', linewidth=line_width)
+  plt.setp(bp_position['medians'], color='blue', linewidth=line_width)
   ax2 = ax1.twinx()
   ax2.set_ylabel('RMSE Orientation [$^\circ$]', color='g')
   ax2.tick_params('y', colors='g')
   bp_orientation = ax2.boxplot(
       orientation_rmses, 0, '', positions=x_ticks + spacing, widths=width)
-  plt.setp(bp_orientation['boxes'], color='green')
-  plt.setp(bp_orientation['whiskers'], color='green')
-  plt.setp(bp_orientation['fliers'], color='red', marker='+')
+  plt.setp(bp_orientation['boxes'], color='green', linewidth=line_width)
+  plt.setp(bp_orientation['whiskers'], color='green', linewidth=line_width)
+  plt.setp(bp_orientation['fliers'], color='green',
+           marker='+')
+  plt.setp(bp_orientation['caps'], color='green', linewidth=line_width)
+  plt.setp(bp_orientation['medians'], color='green', linewidth=line_width)
 
   plt.xticks(x_ticks, methods)
   plt.xlim(x_ticks[0] - 2.5 * spacing, x_ticks[-1] + 2.5 * spacing)
@@ -49,7 +61,6 @@ def generate_time_plot(methods, datasets, runtimes_per_method, colors):
 
   width = 0.6 / num_methods / num_datasets
   spacing = 0.4 / num_methods / num_datasets
-  line_width = 2
   fig, ax1 = plt.subplots()
   ax1.set_ylabel('Time [s]', color='b')
   ax1.tick_params('y', colors='b')
@@ -203,6 +214,7 @@ if __name__ == '__main__':
   print("Creating plots for the following datasets:\n{}".format(datasets))
   for dataset in datasets:
     generate_box_plot(
+        dataset,
         methods,
         [position_rmses_per_method[dataset][method] for method in methods],
         [orientation_rmses_per_method[dataset][method] for method in methods])
