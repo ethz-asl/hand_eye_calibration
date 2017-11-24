a9ef797527bc6a52aa41ff1e30f08039bba449ef
Fadri Furrer <ffurrer@gmail.com>
2017-11-24 13:54:59 +0100

    format setup.py correctly

--- hand_eye_calibration/setup.py
+++ hand_eye_calibration/setup.py
@@ -6,7 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # Fetch values from package.xml.
 setup_args = generate_distutils_setup(
     packages=['hand_eye_calibration'],
-    package_dir={'': 'python'},
-)
+    package_dir={'': 'python'}, )
 
 setup(**setup_args)
