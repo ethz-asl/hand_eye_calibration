7d98a760c61fe6e0149e93e3d9fc6133d8206b3c
mobmi <ethridgeback@gmail.com>
2018-01-10 12:26:14 +0100

    adapted depth_gun config

--- hand_eye_calibration/scripts/depth_gun.sh
+++ hand_eye_calibration/scripts/depth_gun.sh
@@ -1,1 +1,2 @@
-rosrun hand_eye_calibration bag_to_csv_with_config.py --yaml_config_file ~/catkin_ws/src/hand_eye_calibration/hand_eye_calibration/configs/depth_gun_vicon.yaml
+rosrun hand_eye_calibration bag_to_csv_with_config.py \
+--yaml_config_file ~/catkin_ws/src/hand_eye_calibration/hand_eye_calibration/configs/depth_gun_vicon.yaml
